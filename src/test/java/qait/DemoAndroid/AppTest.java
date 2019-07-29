5eff4e2411a75da717d4264a756fa2c1e49b1b9f
HarshitQait <51814447+HarshitQait@users.noreply.github.com>
2019-07-29 18:06:34 +0530

    Add files via upload

--- src/test/java/qait/DemoAndroid/AppTest.java
+++ src/test/java/qait/DemoAndroid/AppTest.java
@@ -6,29 +6,29 @@ import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
-
+import org.testng.annotations.Test;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 public class AppTest {
 
-	public static void main(String[] args) {
+	@Test
+	public void TestChromeLaunch() {
 		
 		//Set the Desired Capabilities
 		DesiredCapabilities caps = new DesiredCapabilities();
 		caps.setCapability("deviceName", "My Phone");
-		caps.setCapability("udid", "emulator-5554"); //Give Device ID of your mobile phone
+		caps.setCapability("udid", "emulator-5554"); //emulator id obtained from adb devices from cmd
 		caps.setCapability("platformName", "Android");
-		caps.setCapability("platformVersion", "7.0");
+		caps.setCapability("platformVersion", "7.0");// obtained from the mobile phone
 		caps.setCapability("browserName", "Chrome");
 		caps.setCapability("noReset", true);
 		
-		//Set ChromeDriver location
+		//Set ChromeDriver location from the local file
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\harshitsrivastava\\Downloads\\chromedriver.exe");
 		
-		//Instantiate Appium Driver
+		//Instantiating Appium Driver
 		AppiumDriver<MobileElement> driver = null;
 		try {
 			driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"), caps);
